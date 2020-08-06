a4f974c77270161fc6109b30605a20ae775a8cf5
mike <mike@lcd-outlet.com>
2020-08-06 01:11:05 +0000

    basic view/model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Graph(models.Model):
+    idx = models.IntegerField()
+
--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+class GraphView(ListView):
+
+    def get(self,request,*args,**kwargs):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        pass
