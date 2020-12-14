2d3667a4544db0f254e035d80b9ca85e7e523e1a
mike <mike@lcd-outlet.com>
2020-12-14 18:09:59 +0000

    typo

--- Installer/start_services.sh
+++ Installer/start_services.sh
@@ -4,8 +4,8 @@ cd ../
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-sudo mv /opt/graph_search/Installer/configs/configs/nginx.conf /etc/nginx/nginx.conf
-sudo mv c/opt/graph_search/Installer/configs/configs/nginx_graph_search.conf /etc/nginx/default
+sudo mv /opt/graph_search/Installer/configs/nginx.conf /etc/nginx/nginx.conf
+sudo mv /opt/graph_search/Installer/configs/nginx_graph_search.conf /etc/nginx/default
 sudo service nginx start
 
 screen -d -m -S django python3 manage.py runserver 0.0.0.0:8000
