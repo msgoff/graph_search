366cc38272f4b57dafe5cbb1a32b514fdf3ab986
mike <mike@lcd-outlet.com>
2020-12-14 20:25:15 +0000

    nginx_config

--- Installer/start_services.sh
+++ Installer/start_services.sh
@@ -5,7 +5,7 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 
 sudo cp /opt/graph_search/Installer/configs/nginx.conf /etc/nginx/nginx.conf
-sudo cp /opt/graph_search/Installer/configs/nginx_graph_search.conf /etc/nginx/default
+sudo cp /opt/graph_search/Installer/configs/nginx_graph_search.conf /etc/nginx/sites-enabled/default
 sudo service nginx start
 
 screen -d -m -S django python3 manage.py runserver 0.0.0.0:8000
