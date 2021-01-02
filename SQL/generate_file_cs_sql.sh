4b2c47f3fa241e1aa93a71d166cecea1a3d269b8
mike <mike@lcd-outlet.com>
2021-01-02 02:53:56 +0000

    generate sql

--- SQL/generate_file_cs_sql.sh
+++ SQL/generate_file_cs_sql.sh
@@ -1,1 +1,1 @@
-find sympy/ -type f -exec sha1sum "{}" \;|xargs -i echo insert into file_checksums \(sha1,filepath\) values "('{}');" |sed "s/  /\',\'/g" > file_checksums.sql
+find ~/sympy/ -type f -exec sha1sum "{}" \;|xargs -i echo insert into file_checksums \(sha1,filepath\) values "('{}');" |sed "s/  /\',\'/g" > file_checksums.sql
